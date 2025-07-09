72db5f809dac2ea0dbb8d6a508f59f8ce41d9256
yagmurcetintas <yagmurcetin@gmail.com>
2025-07-09 15:21:05 -0400

    fix: styling & add page title

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -10,7 +10,7 @@ const Tooltip = ({
   return (
     <div className="relative group ">
       {children}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-yellow-800 text-white text-xs rounded p-4 z-10 whitespace-pre">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-[url(/src/assets/paper.jpg)] text-white text-xs rounded p-4 z-10 whitespace-pre leading-2">
         {text}
       </div>
     </div>
